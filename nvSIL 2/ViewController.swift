b10468780dfbb907da9ea1ec379ca577035e89b0
Ian Garnett (Puck) <ttpuck@icloud.com>
2026-02-03 17:07:28 -0500

    Initial Commit

---
+++ nvSIL 2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  nvSIL 2
+//
+//  Created by Ian Garnett on 2/3/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ nvSIL 2UITests/nvSIL_2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  nvSIL_2UITestsLaunchTests.swift
+//  nvSIL 2UITests
+//
+//  Created by Ian Garnett on 2/3/26.
+//
+
+import XCTest
+
+final class nvSIL_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
